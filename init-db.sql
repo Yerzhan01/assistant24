ddc7e5056f2d765ff904922c1fe28cb0e5fe1545
Yerzhan <erzhansonar@gmail.com>
2026-01-03 22:57:02 +0500

    cleanup: remove Dockerfile.prod, init-db.sql, infra folder

--- init-db.sql
+++
@@ -1,7 +0,0 @@
--- Create dev database if not exists
-SELECT 'CREATE DATABASE assistant24_dev'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assistant24_dev')\gexec
-
--- Create prod database if not exists
-SELECT 'CREATE DATABASE assistant24_prod'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assistant24_prod')\gexec
